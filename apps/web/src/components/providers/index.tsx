1328fc777ce9fc593e5df75977daae5fb95be13d
Manuekle <elustondo129@gmail.com>
2026-05-19 17:13:17 -0500

    [0 q[?1004l[?2004l[?1049l[?25h

--- apps/web/src/components/providers/index.tsx
+++ apps/web/src/components/providers/index.tsx
@@ -1,7 +1,5 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
-import { RouterProvider } from 'react-aria-components';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
 import { QueryProvider } from './query-provider';
 import { AuthProvider } from './auth-provider';
@@ -9,25 +7,21 @@ import { SocketProvider } from './socket-provider';
 import { Toaster } from 'sonner';
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  const router = useRouter();
-
   return (
-    <RouterProvider navigate={router.push}>
-      <NextThemesProvider
-        attribute="data-theme"
-        defaultTheme="system"
-        enableSystem
-        disableTransitionOnChange
-      >
-        <QueryProvider>
-          <AuthProvider>
-            <SocketProvider>
-              <Toaster richColors position="top-right" />
-              {children}
-            </SocketProvider>
-          </AuthProvider>
-        </QueryProvider>
-      </NextThemesProvider>
-    </RouterProvider>
+    <NextThemesProvider
+      attribute="data-theme"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <QueryProvider>
+        <AuthProvider>
+          <SocketProvider>
+            <Toaster richColors position="top-right" />
+            {children}
+          </SocketProvider>
+        </AuthProvider>
+      </QueryProvider>
+    </NextThemesProvider>
   );
 }
